6bc55a99af979b6daf97beebcf644ab9b3008f77
chencheng.kian <chencheng.kian@bytedance.com>
2021-07-18 16:37:31 +0800

    init

---
+++ client.go
@@ -0,0 +1,263 @@
+package unirest
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"net/url"
+	"reflect"
+	"unsafe"
+)
+
+func copyMap(m map[string][]string) map[string][]string {
+	clone := map[string][]string{}
+	for key, values := range m {
+		clone[key] = make([]string, len(values))
+		copy(clone[key], values)
+	}
+	return clone
+}
+
+type HTTPClient struct {
+	query     url.Values
+	form      url.Values
+	files     []*fileField
+	url       string
+	body      []byte
+	method    string
+	header    http.Header
+	basicAuth [2]string
+}
+
+type fileField struct {
+	key      string
+	filename string
+	content  []byte
+}
+
+func New(rawUrl string) *HTTPClient {
+	return &HTTPClient{
+		url:    rawUrl,
+		query:  url.Values{},
+		form:   url.Values{},
+		method: "GET",
+		header: http.Header{},
+		files:  make([]*fileField, 0),
+	}
+}
+
+func (c *HTTPClient) clone() *HTTPClient {
+	clone := *c
+	clone.query = copyMap(c.query)
+	clone.header = copyMap(c.header)
+	clone.form = copyMap(c.form)
+	return &clone
+}
+
+func (c *HTTPClient) AddQuery(key, value string) *HTTPClient {
+	clone := c.clone()
+	clone.query.Add(key, value)
+	return clone
+}
+
+func (c *HTTPClient) AddHeader(key, value string) *HTTPClient {
+	clone := c.clone()
+	clone.header.Add(key, value)
+	return clone
+}
+
+func (c *HTTPClient) AddFormField(key, value string) *HTTPClient {
+	clone := c.clone()
+	clone.form.Add(key, value)
+	clone.method = "POST"
+	return clone
+}
+
+func (c *HTTPClient) AddFile(key, filename string, content []byte) *HTTPClient {
+	clone := c.clone()
+	clone.files = append(c.files, &fileField{
+		key:      key,
+		filename: filename,
+		content:  content,
+	})
+	clone.method = "POST"
+	return clone
+}
+
+func (c *HTTPClient) SetBasicAuth(username, password string) *HTTPClient {
+	clone := c.clone()
+	clone.basicAuth[0] = username
+	clone.basicAuth[1] = password
+	return clone
+}
+
+func (c *HTTPClient) SetJSONBody(json []byte) *HTTPClient {
+	clone := c.clone()
+	clone.body = json
+	clone.header.Set("Content-Type", "application/json")
+	clone.method = "POST"
+	return clone
+}
+
+func (c *HTTPClient) SetRawBody(body []byte) *HTTPClient {
+	clone := c.clone()
+	clone.body = body
+	clone.method = "POST"
+	clone.header.Del("Content-Type")
+	return clone
+}
+
+func (c *HTTPClient) Get() *HTTPClient {
+	clone := c.clone()
+	clone.method = "GET"
+	return clone
+}
+
+func (c *HTTPClient) Post() *HTTPClient {
+	clone := c.clone()
+	clone.method = "POST"
+	return clone
+}
+
+func (c *HTTPClient) Send() (*http.Response, error) {
+	req, err := c.ParseRequest()
+	if err != nil {
+		return nil, err
+	}
+
+	var httpclient http.Client
+	resp, err := httpclient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+func (c *HTTPClient) ParseRequest() (*http.Request, error) {
+	req := &http.Request{
+		Method: c.method,
+		Header: c.header,
+	}
+
+	req.Header.Set("User-Agent", "Unirest-Go/1.0")
+
+	if c.basicAuth[0] != "" {
+		req.SetBasicAuth(c.basicAuth[0], c.basicAuth[1])
+	}
+
+	u, err := url.Parse(c.url)
+	if err != nil {
+		return nil, err
+	}
+	req.URL = u
+
+	if len(c.query) != 0 {
+		req.URL.RawQuery = c.query.Encode()
+	}
+
+	if c.body != nil && (len(c.files) != 0 || len(c.form) != 0) {
+		return nil, errors.New("unirest-go: can send this request with multiple content type")
+	}
+
+	var reader *bytes.Reader
+	if c.body != nil {
+		reader = bytes.NewReader(c.body)
+	} else if len(c.files) != 0 {
+		body := &bytes.Buffer{}
+		writer := multipart.NewWriter(body)
+
+		for _, file := range c.files {
+			fw, err := writer.CreateFormFile(file.key, file.filename)
+			if err != nil {
+				return nil, err
+			}
+			_, err = fw.Write(file.content)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		for key, values := range c.form {
+			for _, value := range values {
+				err := writer.WriteField(key, value)
+				if err != nil {
+					return nil, err
+				}
+			}
+		}
+
+		err := writer.Close()
+		if err != nil {
+			return nil, err
+		}
+
+		reader = bytes.NewReader(body.Bytes())
+		req.Header.Set("Content-Type", writer.FormDataContentType())
+	} else if len(c.form) != 0 {
+		reader = bytes.NewReader(s2b(c.form.Encode()))
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	}
+
+	if reader != nil {
+		readCloser := ioutil.NopCloser(reader)
+		req.Body = readCloser
+		req.ContentLength = int64(reader.Len())
+		snapshot := *reader
+		req.GetBody = func() (io.ReadCloser, error) {
+			r := snapshot
+			return ioutil.NopCloser(&r), nil
+		}
+	}
+	return req, nil
+}
+
+func (c *HTTPClient) AsBytes() ([]byte, error) {
+	resp, err := c.Send()
+	if err != nil {
+		return nil, err
+	}
+
+	buf, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return buf, nil
+}
+
+func (c *HTTPClient) AsString() (string, error) {
+	buf, err := c.AsBytes()
+	if err != nil {
+		return "", err
+	}
+
+	return b2s(buf), nil
+}
+
+// b2s converts byte slice to a string without memory allocation.
+// See https://groups.google.com/forum/#!msg/Golang-Nuts/ENgbUzYvCuU/90yGx7GUAgAJ .
+//
+// Note it may break if string and/or slice header will change
+// in the future go versions.
+func b2s(b []byte) string {
+	/* #nosec G103 */
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+// s2b converts string to a byte slice without memory allocation.
+//
+// Note it may break if string and/or slice header will change
+// in the future go versions.
+func s2b(s string) (b []byte) {
+	/* #nosec G103 */
+	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	/* #nosec G103 */
+	sh := *(*reflect.StringHeader)(unsafe.Pointer(&s))
+	bh.Data = sh.Data
+	bh.Len = sh.Len
+	bh.Cap = sh.Len
+	return b
+}
---
+++ client_test.go
@@ -0,0 +1,221 @@
+package unirest
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSimpleQuery(t *testing.T) {
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		expected := true
+		query := r.URL.Query()
+		v, ok := query["field1"]
+		expected = expected && ok
+		expected = expected && v[0] == "1"
+
+		if expected {
+			w.Write([]byte("true"))
+		} else {
+			w.Write([]byte("false"))
+		}
+	}))
+	defer svr.Close()
+	c, err := New(svr.URL).AddQuery("field1", "1").AsString()
+	assert.NoError(t, err)
+	assert.Equal(t, "true", c)
+}
+
+func TestMultiQuery(t *testing.T) {
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		expected := true
+		query := r.URL.Query()
+		v, ok := query["field1"]
+		expected = expected && ok
+		expected = expected && v[0] == "1"
+		expected = expected && v[1] == "2"
+
+		v, ok = query["field2"]
+		expected = expected && ok
+		expected = expected && v[0] == "1"
+
+		if expected {
+			w.Write([]byte("true"))
+		} else {
+			w.Write([]byte("false"))
+		}
+	}))
+	defer svr.Close()
+	c, err := New(svr.URL).
+		AddQuery("field1", "1").
+		AddQuery("field1", "2").
+		AddQuery("field2", "1").
+		AsString()
+
+	assert.NoError(t, err)
+	assert.Equal(t, "true", c)
+}
+
+func TestSimpleForm(t *testing.T) {
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		expected := true
+		vv := r.PostFormValue("field2")
+		expected = expected && vv == "1"
+
+		v, ok := r.PostForm["field1"]
+		expected = expected && ok
+		expected = expected && v[0] == "1"
+		expected = expected && v[1] == "2"
+
+		if expected {
+			w.Write([]byte("true"))
+		} else {
+			w.Write([]byte("false"))
+		}
+	}))
+	defer svr.Close()
+	c, err := New(svr.URL).
+		AddFormField("field1", "1").
+		AddFormField("field1", "2").
+		AddFormField("field2", "1").
+		AsString()
+
+	assert.NoError(t, err)
+	assert.Equal(t, "true", c)
+}
+
+func TestSimpleFile(t *testing.T) {
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		expected := true
+		_, f, err := r.FormFile("file2")
+		if err != nil {
+			panic(err)
+		}
+
+		expected = expected && f.Filename == "file2.txt"
+		file, _ := f.Open()
+		b, _ := ioutil.ReadAll(file)
+		expected = expected && string(b) == ("file2")
+
+		fs := r.MultipartForm.File["file1"]
+		file, _ = fs[0].Open()
+		b, _ = ioutil.ReadAll(file)
+		expected = expected && fs[0].Filename == "file1.txt"
+		expected = expected && string(b) == ("file1")
+
+		file, _ = fs[1].Open()
+		b, _ = ioutil.ReadAll(file)
+		expected = expected && fs[1].Filename == "file11.txt"
+		expected = expected && string(b) == ("file11")
+
+		if expected {
+			w.Write([]byte("true"))
+		} else {
+			w.Write([]byte("false"))
+		}
+	}))
+	defer svr.Close()
+	c, err := New(svr.URL).
+		AddFile("file1", "file1.txt", []byte("file1")).
+		AddFile("file1", "file11.txt", []byte("file11")).
+		AddFile("file2", "file2.txt", []byte("file2")).
+		AsString()
+
+	assert.NoError(t, err)
+	assert.Equal(t, "true", c)
+}
+
+func TestSimpleBody(t *testing.T) {
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		expected := true
+		b, _ := ioutil.ReadAll(r.Body)
+		expected = expected && string(b) == "raw body"
+
+		if expected {
+			w.Write([]byte("true"))
+		} else {
+			w.Write([]byte("false"))
+		}
+	}))
+	defer svr.Close()
+	c, err := New(svr.URL).SetRawBody([]byte("raw body")).AsString()
+	assert.NoError(t, err)
+	assert.Equal(t, "true", c)
+}
+
+func TestSimpleJSON(t *testing.T) {
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		expected := true
+		b, _ := ioutil.ReadAll(r.Body)
+		expected = expected && string(b) == "{\"A\":1}"
+		expected = expected && r.Header.Get("Content-Type") == "application/json"
+
+		if expected {
+			w.Write([]byte("true"))
+		} else {
+			w.Write([]byte("false"))
+		}
+	}))
+	defer svr.Close()
+	c, err := New(svr.URL).SetJSONBody([]byte("{\"A\":1}")).AsString()
+	assert.NoError(t, err)
+	assert.Equal(t, "true", c)
+}
+
+func TestBodyAndJSON(t *testing.T) {
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		expected := true
+		b, _ := ioutil.ReadAll(r.Body)
+		expected = expected && string(b) == "raw body"
+		expected = expected && r.Header.Get("Content-Type") == ""
+
+		if expected {
+			w.Write([]byte("true"))
+		} else {
+			w.Write([]byte("false"))
+		}
+	}))
+	defer svr.Close()
+	c, err := New(svr.URL).SetJSONBody([]byte("{\"A\":1}")).SetRawBody([]byte("raw body")).AsString()
+	assert.NoError(t, err)
+	assert.Equal(t, "true", c)
+}
+
+func TestClone(t *testing.T) {
+	c := New("a.com")
+	c2 := c.Post()
+	assert.Equal(t, "GET", c.method)
+	assert.Equal(t, "POST", c2.method)
+
+	c3 := c.AddHeader("Q", "1")
+	assert.Equal(t, 0, len(c.header["Q"]))
+	assert.Equal(t, "1", c3.header["Q"][0])
+
+	c4 := c.AddQuery("q", "1")
+	assert.Equal(t, 0, len(c.query["q"]))
+	assert.Equal(t, "1", c4.query["q"][0])
+
+	c5 := c.SetRawBody([]byte("123"))
+	assert.Equal(t, 0, len(c.body))
+	assert.Equal(t, "123", string(c5.body))
+
+	c6 := c.SetBasicAuth("1", "2")
+	assert.Equal(t, "", c.basicAuth[0])
+	assert.Equal(t, "", c.basicAuth[1])
+	assert.Equal(t, "1", c6.basicAuth[0])
+	assert.Equal(t, "2", c6.basicAuth[0])
+
+	c7 := c.AddFile("1", "2", []byte("3"))
+	assert.Equal(t, 0, len(c.files))
+	assert.Equal(t, 1, len(c7.files))
+
+	return
+}
+
+func TestExpectedError(t *testing.T) {
+	_, err := New("a.com").AddFormField("1", "1").SetRawBody([]byte("123")).AsBytes()
+	assert.Error(t, err, "unirest-go: can send this request with multiple content type")
+}
